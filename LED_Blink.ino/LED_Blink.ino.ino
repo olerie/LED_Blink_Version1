69d9f10af9dcc43cc18c30cc6e830ce18997b6fe
olerie <71316118+olerie@users.noreply.github.com>
2020-09-15 11:10:33 +0200

    Pin 3 als gruene LED definiert
    Gruene LED

--- LED_Blink.ino/LED_Blink.ino.ino
+++ LED_Blink.ino/LED_Blink.ino.ino
@@ -1,4 +1,4 @@
-+/*
+/*
  *Funktion: LED blinkt im ein Seknuden takt.
  *Programierer: Ole Riehemann
  *letzte Änderung: 11.09.2020
@@ -9,11 +9,11 @@
  */
  
 
-
+#define grueneLED 3
 
 void setup()
 {
-  pinMode(9, OUTPUT);    //LED grün
+  pinMode(grueneLED, OUTPUT);    //LED grün
   Serial.begin(9600);    //Zur Kontrolle
 
 }
@@ -21,10 +21,10 @@ void setup()
 void loop()
 {
   /************* LED im Sekundentakt an und aus *************/
-  digitalWrite(3, HIGH);     // LED An
+  digitalWrite(grueneLED, HIGH);     // LED An
   Serial.println("LED an");
   delay(1000);               //Sekundetakt
-  digitalWrite(3, LOW);
+  digitalWrite(grueneLED, LOW);
   Serial.println("LED aus");
   delay(1000);
 
